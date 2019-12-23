6205fe67fa2f1b4c219772539af90099994d7102
Sidian.Luo <Sidian.Luo@geely.com>
2019-12-23 14:27:55 +0800

    补充

--- bashrc-wsl.sh
+++ bashrc-wsl.sh
@@ -9,7 +9,7 @@ export hosts_home=/mnt/c/Windows/System32/drivers/etc # 配置hosts环境变量
 export notes=$home/Documents/blogs/sidian123/notes # 笔记路径
 export PATH=/home/$USER/bin:$PATH # 将wsl家目录中的bin/目录加入到环境变量中
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64 # Java
-export typora_home="$Win_Home/Program Files/Typora"
+export typora_home="$Win_Home/Program Files/Typora" # typora
 
 # 当打开终端并且位于Linux家目录时,则切换到Windows的家目录中.
 # 这里考虑到了作为VSCode默认终端的情况.
@@ -24,6 +24,8 @@ alias open='explorer.exe .'
 # 方便连接服务器
 alias remote-sh='ssh root@sh.sidian.live'
 alias remote-hk='ssh root@hk.sidian.live'
+alias proxy-sh='ssh -D 1024 root@sh.sidian.live'
+alias proxy-hk='ssh -D 1025 root@hk.sidian.live'
 # 方便使用typora
 function typora () {
   "$typora_home/bin/typora.exe" "$*" >/dev/null 2>&1 &
