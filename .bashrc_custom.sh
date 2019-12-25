d5128bb9f18854e443a3e6dda190c921cbf8771d
sidian <sidian123@qq.com>
2019-12-25 21:22:25 +0800

    替换成Git Bash的配置

--- .bashrc_custom.sh
+++ .bashrc_custom.sh
@@ -1,19 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # 环境变量
-export user=sidian #用户名
-export Win_Home=/mnt/c # windows家目录
+export USER=sidian #用户名
+export Win_Home=/c # windows家目录
 
-export home=$Win_Home/Users/$user #设置Windows家目录的环境变量
-export HOME=$home # 当cd时,可直接跳转到Windows家目录
-export hosts_home=/mnt/c/Windows/System32/drivers/etc # 配置hosts环境变量
-export notes=$home/Documents/blogs/sidian123/notes # 笔记路径
-export PATH=/home/$USER/bin:$PATH # 将wsl家目录中的bin/目录加入到环境变量中
+export notes=$HOME/Documents/blogs/sidian123/notes # 笔记路径
 export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64 # Java
 export typora_home="$Win_Home/Program Files/Typora" # typora
-
-# 当打开终端并且位于Linux家目录时,则切换到Windows的家目录中.
-# 这里考虑到了作为VSCode默认终端的情况.
-[[ $PWD == "$HOME" ]] && cd "$home"
 
 # 取消ls高亮,改用后缀方式
 alias ls="ls --file-type"
